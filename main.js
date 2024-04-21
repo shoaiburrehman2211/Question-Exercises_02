08d19095a86a5fc5e9984af6ee630f636abea8fe
Shoaib-ur-Rehman <160622745+shoaiburrehman2211@users.noreply.github.com>
2024-04-22 02:12:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+/*store a person's name in a variable,and print a message to that person.your message should
+ be simple,such as,"Hello Eric,would you like to learn some Python today?"
+*/
+var personName = "shoaib";
+console.log("Hello" + personName + ", would you like to learn some python today?");
